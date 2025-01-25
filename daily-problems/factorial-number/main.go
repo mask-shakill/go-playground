5e48b52633ece7a9a751955fec5edeead848b1d3
Shahariar Kobir Shakil <mask.shakill@gmail.com>
2025-01-25 16:33:33 +0600

    added daily problems in factorial number

---
+++ daily-problems/factorial-number/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+    if n == 0 {
+        return 1
+    }
+    return n * factorial(n-1)
+}
+
+func main() {
+    var num int
+    fmt.Println("Enter a number:")
+    fmt.Scan(&num)
+
+    fmt.Printf("Factorial of %d is: %d\n", num, factorial(num))
+}
